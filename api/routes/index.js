315562a7f9a6886b985254e15f9c4a8416c25ff7
Carol Brown <carolbrown@Carols-MacBook-Air.local>
2023-10-22 15:20:27 +0100

    cage.model, cage.controller, cage.router done funcionando

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router()
 
 //importamos rutas
 const animalRouter = require('./animal.router')
+const cageRouter = require('./cage.router')
 
 //definimos rutas
 router.use('/animal', animalRouter)
+router.use('/cage', cageRouter)
 
 //exportamos modulo
 module.exports = router
