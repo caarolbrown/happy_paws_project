143d21343dcdbd31fcb69f2b3da5c12399a976de
Mahinsouru <57689217+Aythami-I-G@users.noreply.github.com>
2023-10-22 22:12:08 +0100

    Added payroll table

---
+++ happy_paws_project-Payroll/api/models/payroll.model.js
@@ -0,0 +1,25 @@
+const { sequelize } = require('../../database/index.js') 
+const { DataTypes } = require('sequelize')
+
+const Payroll = sequelize.define('payroll', { 
+
+        id_user: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        amount: {
+            type: DataTypes.INTEGER,
+            allowNull: null
+        },
+        payment_day: {
+            type: DataTypes.DATE,
+        },
+        position: {
+            type: DataTypes.STRING,
+        }
+},
+{ 
+    timestamps: false, 
+})
+
+module.exports = Payroll
---
+++ happy_paws_project-Payroll/index.js
@@ -0,0 +1,29 @@
+const { checkDB, syncModels } = require('./database/index.js')
+const { setRelations } = require('./database/models')
+const express = require ('express')
+const cors = require ('cors')
+const morgan = require ('morgan')
+
+require ('dotenv').config()
+
+async function connectDB(){
+    await checkDB() 
+    setRelations()
+    await syncModels() 
+}
+
+function launchServer(){
+    const app = express()
+    .use(cors())
+    .use(morgan('dev'))
+    .use(express.json())
+    .use('/api', require('./api/routes.index.js'))
+    .listen(process.env.SRV_PORT, () => { console.log ('Express server listening on port 3000')})
+}
+
+async function startAPI(){
+    connectDB()
+    launchServer()
+}
+
+startAPI()
