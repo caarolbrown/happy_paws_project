e37dc058886c061c92c9b25dd02558ca9e3c5e03
Aythami-I-G <aythasg@gmail.com>
2023-10-23 17:00:15 +0100

    done relationships done

--- database/models.js
+++ database/models.js
@@ -5,12 +5,34 @@ const Sickness = require('../api/models/sickness.model')
 const Animal = require('../api/models/animal.model')
 const Cage = require('../api/models/cage.model')
 const Payroll = require ('../api/models/payroll.model.js')
+const HostFamily = require('../api/models/hostfamily.model.js')
+const AdoptiveFamily = require ('../api/models/adoptiveFamily.model.js')
 
 function setRelations(){
     try {
         //One to one 
         Animal.hasOne(Cage)
         Cage.belongsTo(Animal)
+
+        User.hasOne(Payroll)
+        Payroll.belongsTo(User)
+
+        User.hasOne(HostFamily)
+        HostFamily.belongsTo(User)
+
+        User.hasOne(AdoptiveFamily)
+        AdoptiveFamily.belongsTo(User)
+
+        //One to many
+
+        User.hasMany(Task)
+        Task.belongsTo(User)
+
+        HostFamily.hasMany(User)
+        User.belongsTo(HostFamily)
+
+        Animal.hasMany(AdoptiveFamily)
+        AdoptiveFamily.belongsTo(Animal)
 
         //Many to many
         Animal.belongsToMany(Treatment, { through: 'animal_treatment' })
@@ -22,7 +44,6 @@ function setRelations(){
         User.hasOne(Payroll)
         Payroll.belongsTo(User)
           
-        console.log('Done')
         User.hasMany(Task);
         Task.belongsTo(User);
     } catch (error) {
