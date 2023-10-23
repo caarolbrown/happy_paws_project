a93085623e03d66644b61611cfe3b58ff784f8e1
fernando <fernando_1991_21@hotmail.com>
2023-10-23 14:52:29 +0100

    userDone

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -79,5 +79,21 @@ async function setTask(req, res) {
   }
 }
 
-async function 
+/*async function getPayroll(req, res) {
+    try {
+        const user = await User.findByPk(req.body.userId, {
+            include: Payroll
+        })
+        if(!user) {
+            return res.status(404).json({error: 'Usuario no encontrado.'})    
+        }
+        if (user.role !== 'employee') {
+            return res.status(403).json({ error: 'Solo los empleados pueden acceder a payroll'})
+        }
+        res.status(200).json(user.payroll)
+     } catch (error) {
+        res.status(500).send(error.message)
+        
+    }
+}*/
 module.exports = { getAllUsers, getOneUser, createUser, updateUser, deleteUser, getProfile, setTask }
