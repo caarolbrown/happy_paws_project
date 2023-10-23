3af3f25295b5d435ecd5e0260da80b0b2aaeb6da
Aythami-I-G <aythasg@gmail.com>
2023-10-23 14:58:54 +0100

    test for merge in future

---
+++ api/models/adoptiveFamily.model.js
@@ -0,0 +1,26 @@
+const { sequelize } = require('../../database/index') 
+const { DataTypes } = require('sequelize')
+
+const AdoptiveFamily = sequelize.define('adoptiveFamily', { 
+
+        id_user: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        address: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        phone: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        interview: {
+            type: DataTypes.STRING,
+        }
+},
+{ 
+    timestamps: false, 
+})
+
+module.exports = AdoptiveFamily
--- api/routes/index.js
+++ api/routes/index.js
@@ -5,12 +5,18 @@ const animalRouter = require('./animal.router')
 const cageRouter = require('./cage.router')
 const treatmentRouter = require('./treatment.router')
 const sicknessRouter = require('./sickness.router')
+const adoptiveFamily = require('./adoptiveFamily.router')
+const payroll = require('./payroll.router')
+const hostFamily = require('./hostfamily.routes')
+
 
 //definimos rutas
 router.use('/animal', animalRouter)
 router.use('/cage', cageRouter)
 router.use('/treatment', treatmentRouter)
 router.use('/sickness', sicknessRouter)
-
+router.use('/adoptiveFamily', adoptiveFamily)
+router.use('/payroll', payroll)
+router.use('/hostFamily', hostFamily)
 //exportamos modulo
 module.exports = router
