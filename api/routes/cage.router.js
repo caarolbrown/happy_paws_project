029be2ccec184b4dcf34d5f458c9e746e31e524c
Carol <carolinamorenofdez@gmail.com>
2023-10-24 16:18:01 +0100

    router cage done

--- api/routes/cage.router.js
+++ api/routes/cage.router.js
@@ -1,13 +1,14 @@
 const { getAllCages, getOneCage, createCage, updateCage, deleteCage, cageStatus } = require('../controllers/cage.controller')
+const { checkAuth, checkAdmin, checkEmployee, checkVolunteer } = require('../middleware')
 
 const router = require('express').Router()
 
 //definimos CRUD basico de cage
-router.get('/', getAllCages)
-router.get('/availability', cageStatus)
-router.get('/:id', getOneCage)
-router.post('/', createCage)
-router.put('/:id', updateCage)
-router.delete('/:id', deleteCage)
+router.get('/', checkAuth, checkVolunteer, getAllCages)
+router.get('/availability', checkAuth, checkVolunteer, cageStatus)
+router.get('/:id', checkAuth, checkVolunteer, getOneCage)
+router.post('/', checkAuth, checkAdmin, createCage)
+router.put('/:id', checkAuth, checkAdmin, updateCage)
+router.delete('/:id', checkAuth, checkAdmin, deleteCage)
 
 module.exports = router
