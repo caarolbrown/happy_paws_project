cd62c9c019fe47700aec9d60f61eb71c73b964f2
fernando <fernando_1991_21@hotmail.com>
2023-10-25 14:06:40 +0100

    commit

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -107,22 +107,4 @@ async function getPayroll(req, res) {
     }
 }
 
-// async function getAdoptiveFamilies(req, res) {
-//     try {
-//         const user = await User.findByPk(req.body.userId, {
-//             include: AdoptiveFamily
-//         })
-//         if (!user) {
-//             return res.status(404).json({ error: 'Usuario no encontrado.' })
-//         }
-//         if (user.role !== 'employee' || user.role !== 'admin') {
-//             return res.status(403).json({ error: 'Solo el personal autorizado puede acceder a payroll' })
-//         }
-//         return res.status(200).json(user.payroll)
-//     } catch (error) {
-//         return res.status(500).send(error.message)
-
-//     }
-// }
-
 module.exports = { getAllUsers, getOneUser, createUser, updateUser, deleteUser, getProfile, setTask, getPayroll }
